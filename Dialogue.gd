d3fb19ec010aa4b048797bb2826029b8dd54e880
Sarah Sprague <sprasarah@gmail.com>
2022-07-15 16:44:53 -0500

    part 1 tutorial additions

---
+++ Dialogue.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var dialogue_1 = [
+	{
+		"Name": "Person",
+		"Text": "Hello? Can I help you?",
+		"Choices": ["",""]
+	},
+	{
+		"Name": "Person",
+		"Text": "... are you selling something",
+		"Choices": ["Buy this juicer!", "Can I have a moment of your time?"]
+	}
+]
